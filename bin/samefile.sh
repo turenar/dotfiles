b01f6e99db9c9fc57c7acac37bde0bd63e896c79
Turenar <snswinhaiku.lo@gmail.com>
2012-12-21 13:19:13 +0900

    Imported samefile.sh.20101228

---
+++ bin/samefile.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+MYNAME=`gettail $0`
+MYTMPDIR="/tmp/${MYNAME}.$$"
+INODELISTF="${MYTMPDIR}/inode.lst"
+INODESORTF="${MYTMPDIR}/inode.sorted"
+UNIQINODEF="${MYTMPDIR}/file.uniq"
+HASHF="${MYTMPDIR}/md5sum"
+HASHSORTF="${HASHF}.sorted"
+DEBUGMODE=
+
+STARTDATE=0
+ENDDATE=0
+
+function calcdate(){
+	local datediff
+	let datediff="$2 - $1"
+	echo "took ${datediff}sec" >&2
+}
+
+function destruct(){
+	if [ -z DEBUGMODE ]; then
+		echo "Removing temporary directory" >&2
+		rm -rf ${MYTMPDIR}
+	fi
+	exit 0;
+}
+
+function countline(){
+	local linecount=`wc -l $1 | cut -d ' ' -f1`
+	echo -n "${linecount}files: " >&2
+}
+
+if [ "$1" = "-d" ]; then
+	DEBUGMODE=yes
+fi
+echo "Temporary directory: ${MYTMPDIR}" >&2
+
+mkdir ${MYTMPDIR}
+trap "destruct" 0
+trap "echo 'Caught signal.';exit 1;" INT TERM
+
+STARTDATE=`date '+%s'`
+echo -n "inodelistを作成しています..." >&2
+find . -type f -print0 | xargs -0 ls -Ui1 > ${INODELISTF}
+ENDDATE=`date '+%s'`
+countline ${INODELISTF}
+calcdate ${STARTDATE} ${ENDDATE}
+
+STARTDATE=`date '+%s'`
+echo -n "inodelistをソートしています..." >&2
+#								  ↓スペースとタブ
+sort ${INODELISTF} | sed -e 's/^[ 	]*//g' > ${INODESORTF}
+ENDDATE=`date '+%s'`
+calcdate ${STARTDATE} ${ENDDATE}
+
+STARTDATE=`date '+%s'`
+echo -n "inodelistの重複を取り除いています..." >&2
+BUFIFS=$IFS
+IFS=
+
+PREVFILE=
+PREVINODE=
+NOWFILE=
+NOWINODE=
+
+exec 3< ${INODESORTF}
+exec 4> ${UNIQINODEF}
+
+while read FL 0<&3
+do
+	PREVFILE=${NOWFILE}
+	PREVINODE=${NOWINODE}
+	NOWINODE=`echo ${FL} | cut -d' ' -f1`
+	NOWFILE=`echo ${FL} | cut -d' ' -f2-`
+
+	if [ "${NOWINODE}" != "${PREVINODE}" ]; then
+		echo -n ${NOWFILE} 1>&4
+		echo -ne '\0' 1>&4
+	fi
+done
+exec 3<&-
+exec 4>&-
+
+IFS=$BUFIFS
+
+ENDDATE=`date '+%s'`
+calcdate ${STARTDATE} ${ENDDATE}
+
+STARTDATE=`date '+%s'`
+echo -n "ハッシュを計算しています..." >&2
+xargs -0 md5sum < ${UNIQINODEF} > ${HASHF}
+ENDDATE=`date '+%s'`
+countline ${HASHF}
+calcdate ${STARTDATE} ${ENDDATE}
+
+STARTDATE=`date '+%s'`
+echo -n "ハッシュをソートしています..." >&2
+sort ${HASHF} > ${HASHSORTF}
+ENDDATE=`date '+%s'`
+calcdate ${STARTDATE} ${ENDDATE}
+
+if [ ! -z "${DEBUGMODE}" ]; then
+	echo "ハードリンクをスキップ" >&2
+	exit 0
+fi
+
+echo "同一ファイルをハードリンクしています..." >&2
+
+
+BUFIFS=$IFS
+IFS=
+
+PROCFCOUNT=0
+
+PREVFILE=
+PREVHASH=
+NOWFILE=
+NOWHASH=
+exec 3< ${HASHSORTF}
+while read FL 0<&3
+do
+	PREVFILE=${NOWFILE}
+	PREVHASH=${NOWHASH}
+	NOWHASH=`echo ${FL} | cut -d' ' -f1`
+	NOWFILE=`echo ${FL} | cut -d' ' -f3-`
+	if [ "${NOWHASH}" = "${PREVHASH}" ]; then
+		diff "${NOWFILE}" "${PREVFILE}"
+		ISDIFF=$?
+		if [ ${ISDIFF} -eq 0 ]; then
+			if [ ! -z "${DEBUGMODE}" ]; then			
+				echo "ln -f ${PREVFILE} ${NOWFILE}"
+			fi
+			let PROCFCOUNT="${PROCFCOUNT} + 1"
+			echo -ne "\r"
+			printf "%5d files processed. " ${PROCFCOUNT} >&2
+			ln -f ${PREVFILE} ${NOWFILE}
+		elif [ ${ISDIFF} -eq 2 ]; then
+			echo "different file: ${PREVFILE} ${NOWFILE}" >&2
+		else
+			echo "ERROR: diff returned ${ISDIFF}" >&2
+		fi
+	fi	
+done
+exec 3<&-
+
+IFS=$BUFIFS
+
+ENDDATE=`date '+%s'`
+calcdate ${STARTDATE} ${ENDDATE}
+
+
