716a61b27a8c9ee7cfdace7533a9a99f1f2712f6
Turenar <snswinhaiku.lo@gmail.com>
2013-01-19 09:48:17 +0900

    update sun-jdk.sh

--- bin/sun-jdk.sh
+++ bin/sun-jdk.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
-export GENTOO_VM=sun-jdk-1.6
-export JAVA_HOME=/opt/sun-jdk-1.6.0.37/
+unset GENTOO_VM _vm _no_exec
+_vm=sun-jdk-1.6
+
+while true; do
+	case $1 in
+		-6) _vm=sun-jdk-1.6; shift; continue;;
+		-7) _vm=oracle-jdk-bin-1.7; shift; continue;;
+		-s) _no_exec=1; shift; continue;;
+		-*) echo "[$(basename $0)] Invalid argument: $1">&2; exit 1;;
+		*)  break;;
+	esac
+done
+
+export GENTOO_VM=${_vm}
+export JAVA_HOME=/usr/lib/jvm/${_vm}
 export JDK_HOME=${JAVA_HOME}
-exec $@
+
+if [ -z "${no_exec}" ]; then
+	exec $@
+fi
